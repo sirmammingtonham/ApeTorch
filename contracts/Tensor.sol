1a3f467ae46de54bc6cc954f997f0b1657cd7d97
Ethan Joseph <ethanj217@gmail.com>
2022-03-27 22:56:41 -0700

    bruh

--- contracts/Tensor.sol
+++ contracts/Tensor.sol
@@ -2,66 +2,87 @@
 pragma solidity ^0.8.13;
 
 import "./operations/Operation.sol";
+import "prb-math/contracts/PRBMathSD59x18.sol";
+import "solmate/contracts/MatrixUtils.sol";
 
-contract T {
-    struct Value {
-        int256 data;
-        int256 gradient;
-        bool requires_grad;
-        function(Value memory) internal pure returns (Value memory) backward;
-    }
-
-    function addition(Value memory a, Value memory b)
-        internal
-        pure
-        returns (Value memory)
-    {
-        return
-            Value(
-                a.data + b.data,
-                0,
-                a.requires_grad || b.requires_grad,
-                _addition_backwards
-            );
-    }
-
-    function _addition_backwards(Value memory a)
-        internal
-        pure
-        returns (Value memory)
-    {
-        a.gradient += (a.data > 0 ? int256(1) : int256(0)) * a.gradient;
-        return a;
-    }
-
-    function multiplication(Value memory a, Value memory b) internal {}
-
-    function relu(Value memory a) internal pure returns (Value memory) {
-        return
-            Value(
-                a.data * (a.data > 0 ? int256(1) : int256(0)),
-                0,
-                a.requires_grad,
-                _relu_backwards
-            );
-        // a.data = a.data * (a.data > 0 ? int256(1) : int256(0));
-
-        // if (a.requires_grad) {
-        //     a.backward = _relu_backwards;
-        // }
-
-        // return a;
-    }
-
-    function _relu_backwards(Value memory a)
-        internal
-        pure
-        returns (Value memory)
-    {
-        a.gradient += (a.data > 0 ? int256(1) : int256(0)) * a.gradient;
-        return a;
-    }
+struct Tensor {
+    int256[][] data;
+    int256[][] gradient;
 }
+
+contract TT {
+    using PRBMathSD59x18 for int256;
+    using MatrixUtils for int256[][];
+
+    // struct Tensor {
+    // 	int256[][] data;
+    // 	int256[][] gradient;
+    // }
+
+    function add(Tensor memory a, Tensor memory b)
+        internal
+        pure
+        returns (Tensor memory)
+    {
+        return Tensor(a.data.add(b.data), a.gradient.add(b.gradient));
+    }
+
+    function sub(Tensor memory a, Tensor memory b)
+        internal
+        pure
+        returns (Tensor memory)
+    {
+        return Tensor(a.data.add(b.data.mul(-1)), a.gradient.add(b.gradient.mul(-1)));
+    }
+
+	function dot(Tensor memory a, Tensor memory b)
+        internal
+        pure
+        returns (Tensor memory) {
+		return Tensor(a.data.dot(b.data), )
+	}
+}
+
+interface Function {
+    function forward() external;
+
+    function backward() external;
+    // function getParams() external;
+}
+
+// contract Linear is Function {
+//     Tensor weights;
+//     Tensor bias;
+
+//     function forward(Tensor memory x) public view returns (Tensor memory) {}
+
+//     function backward(Tensor memory dY) public returns (Tensor memory) {}
+
+//     function getParams() public view returns (Tensor[] memory) {}
+// }
+
+// struct Dependency {
+// 	Tensor tensor;
+// 	function () grad_fn;
+// }
+
+// struct Tensor {
+//     int256[][] data;
+//     int256[][] gradient;
+//     bool requires_grad;
+// 	Dependency[] depends_on;
+//     // function () grad_fn;
+// }
+
+// function add(Tensor memory a, Tensor memory b) internal {
+// 	int256[][] memory data = a.data.add(b.data);
+// 	bool requires_grad = a.requires_grad || b.requires_grad;
+// 	// Dependency[] memory depends_on;
+// 	Dependency[2] memory depends_on;
+// 	if (a.requires_grad) {
+// 		depends_on[0] = Dependency(a, )
+// 	}
+// }
 
 // contract Tensor {
 //     int256[][] item;
